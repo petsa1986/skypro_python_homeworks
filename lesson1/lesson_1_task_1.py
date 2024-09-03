20fbd01aa3ba0d674b8cebf14320f9e314e38761
Petsa1986 <Petsane@ya.ru>
2024-09-03 20:17:25 +0300

    lesson3

--- lesson1/lesson_1_task_1.py
+++ lesson1/lesson_1_task_1.py
@@ -1,11 +1,3 @@
-from address import Address
-from mailing import Mailing
+my_name = "Пётр"
 
-to_address = Address('423520', 'Нижнекамск', 'Нефтяников', '65', '56')
-from_address = Address('155800', 'Кинешма', 'Ленина', '34', '1')
-mailing = Mailing(to_address, from_address, 123456, 'ABC123')
-
-print(f"Отправление {mailing.track} из {mailing.from_address.index}, {mailing.from_address.city}, "
-      f"{mailing.from_address.street}, {mailing.from_address.home} - {mailing.from_address.room} в "
-      f"{mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street}, "
-      f"{mailing.to_address.home} - {mailing.to_address.room}. Стоимость {mailing.cost} рублей.")
+print ("Привет, "+ my_name)
