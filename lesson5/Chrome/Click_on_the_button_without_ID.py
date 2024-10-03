918fc3c02cc886998d6a3d0bfdc7fe3c3e426f67
Petsa1986 <Petsane@ya.ru>
2024-10-03 16:07:34 +0300

    five homework

--- lesson5/Chrome/Click_on_the_button_without_ID.py
+++ lesson5/Chrome/Click_on_the_button_without_ID.py
@@ -6,24 +6,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install())
+)
 
 # Запустить сайт
 driver.get(" http://uitestingplayground.com/dynamicid/")
 sleep(5)
 
 # Кликнуть на синюю кнопку(нажать) и в поле, любое место(отжать)
-button = driver.find_element(By.CSS_SELECTOR, "button[class='btn btn-primary']").click()
+button = driver.find_element(
+    By.CSS_SELECTOR, "button[class='btn btn-primary']"
+).click()
 sleep(5)
 field = driver.find_element(By.CSS_SELECTOR, "div[class='container']").click()
 sleep(5)
 
 # Повторение нажатия три раза
 for _ in range(3):
-    button = driver.find_element(By.CSS_SELECTOR, "button[class='btn btn-primary']").click()
+    button = driver.find_element(
+        By.CSS_SELECTOR, "button[class='btn btn-primary']"
+    ).click()
     sleep(5)
-    field = driver.find_element(By.CSS_SELECTOR, "div[class='container']").click()
-    
+    field = driver.find_element(
+        By.CSS_SELECTOR, "div[class='container']"
+    ).click()
+
 sleep(10)
 
 # Закрыть браузер
