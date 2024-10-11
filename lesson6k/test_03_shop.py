d4dd497dea8286ccb0cabbb5b1ed727d77395fb4
Petsa1986 <Petsane@ya.ru>
2024-10-11 12:16:50 +0300

    6k исправление

--- lesson6k/test_03_shop.py
+++ lesson6k/test_03_shop.py
@@ -14,21 +14,23 @@ def test_shop_form(chrome_browser):
     add_to_cart_buttons = [
         (By.ID, "add-to-cart-sauce-labs-backpack"),
         (By.ID, "add-to-cart-sauce-labs-bolt-t-shirt"),
-        (By.ID, "add-to-cart-sauce-labs-onesie")
+        (By.ID, "add-to-cart-sauce-labs-onesie"),
     ]
     for button in add_to_cart_buttons:
         chrome_browser.find_element(*button).click()
 
-    chrome_browser.find_element(By.ID,"shopping_cart_container").click()    
-    chrome_browser.find_element(By.ID,"checkout").click()
+    chrome_browser.find_element(By.ID, "shopping_cart_container").click()
+    chrome_browser.find_element(By.ID, "checkout").click()
 
-    chrome_browser.find_element(By.ID,"first-name").send_keys("Oksana")
-    chrome_browser.find_element(By.ID,"last-name"). send_keys("Novopashina")
-    chrome_browser.find_element(By.ID,"postal-code"). send_keys("155800")
-    chrome_browser.find_element(By.ID,"continue").click()
+    chrome_browser.find_element(By.ID, "first-name").send_keys("Oksana")
+    chrome_browser.find_element(By.ID, "last-name").send_keys("Novopashina")
+    chrome_browser.find_element(By.ID, "postal-code").send_keys("155800")
+    chrome_browser.find_element(By.ID, "continue").click()
 
-    total_price = chrome_browser.find_element(By.CLASS_NAME,"summary_total_label")
+    total_price = chrome_browser.find_element(
+        By.CLASS_NAME, "summary_total_label"
+    )
     total = total_price.text.strip().replace("Total: $", "")
     expected_total = "58.29"
     assert total == expected_total
-    print (f"Итоговая сумма равна $ {total}")
+    print(f"Итоговая сумма равна $ {total}")
